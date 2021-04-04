87e895bdcd667cbdbf9bf473a3133ff1c001cbcd
Luis Eduardo <dududuluis66@gmail.com>
2021-04-03 22:58:25 -0300

    Primeiro commit
    Aqui eu coloquei meu primeiro commit desse curso

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,9 @@
+console.log("Sentença de código")
+
+//Blocos de codigos 
+{
+    {
+        console.log("Olá")
+        console.log("Mundo!") //padrao do curso 
+    }
+}
---
+++ exercicios-js/fundamentos/tipagemFraca.js
@@ -0,0 +1,13 @@
+let qualquer = 'legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+//typeof serve pra ver o tipo do codigo se e string entre outros   
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//evitar nome generico para sigla, exemplo:
+let valor = ''
+let numero = 1
+let pqp = false //produto quimico perigoso... kkk
